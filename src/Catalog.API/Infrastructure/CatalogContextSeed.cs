205e5920217a7c4f54fc013efa8aba2f2dd4550a
tmajaraunga <tmajaraunga@css.edu>
2024-12-23 10:53:56 -0600

    Updated changes to catalog.json

--- src/Catalog.API/Infrastructure/CatalogContextSeed.cs
+++ src/Catalog.API/Infrastructure/CatalogContextSeed.cs
@@ -78,8 +78,8 @@ public partial class CatalogContextSeed(
         public string Type { get; set; }
         public string Brand { get; set; }
         public string Name { get; set; }
-        public string gender { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
+        public string gender { get; set; }
     }
 }
